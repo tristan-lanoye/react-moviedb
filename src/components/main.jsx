39a96c1b28d75c2934b0b6960f06f08a107dac16
tristan-lanoye <tristanyjlanoye@gmail.com>
2017-12-23 06:54:18 +0100

    initial commit

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Display from './display.jsx'
+import Input from './input.jsx'
+
+export default class App extends React.Component {
+    render() {
+        return (<Input/>)
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var path = require('path')
+
+var isProd = process.env.NODE_ENV === 'production'
+var cssDev = ['style-loader', 'css-loader', 'sass-loader']
+var cssProd = ExtractTextPlugin.extract({
+    use: (['css-loader', 'sass-loader']),
+    fallback: 'style-loader',
+    publicPath: '/dist'
+})
+var cssConfig = isProd ? cssProd : cssDev
+
+module.exports = {
+    entry: {
+        'app': './src/app.jsx',
+        'vendor': './src/vendor.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'scripts/[name].bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.s?css$/,
+                use: cssConfig
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file-loader?name=images/[name].[ext]',
+                    // 'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/',
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            libcss: path.resolve(__dirname, './src/lib/css'),
+            libjs: path.resolve(__dirname, './src/lib/js')
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        hot: true,
+        stats: 'errors-only',
+        open: true
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'styles/[name].bundle.css',
+            disable: !isProd,
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Project',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/templates/index.ejs'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new UglifyJsPlugin()
+    ]
+}
